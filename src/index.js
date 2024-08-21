03cc812b4f0ad7101a63365fe4e418c333be71cf
HÁ <haidee.alvarez.c@gmail.com>
2024-08-21 17:16:07 +0100

    update real-time temperature and display it

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+function searchCity(city) {
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=9453eocfb302f861c59f1e9f04d3bta4`;
+  axios.get(apiUrl).then(function refreshWeather(response) {
+    let currentTemp = document.querySelector("#current-temp");
+    currentTemp.innerHTML = Math.round(response.data.temperature.current);
+  });
+}
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", function (event) {
   event.preventDefault();
   let searchBox = document.querySelector("#input-box");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchBox.value;
+  searchCity(searchBox.value);
 });
